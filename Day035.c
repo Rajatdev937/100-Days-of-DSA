41154b428b06e61827f8efc200ca0eaa987546bf
Rajat Negi <007negirajat@gmail.com>
2026-03-18 22:55:06 +0530

    stack

---
+++ Day035.c
@@ -0,0 +1,76 @@
+// Problem: Queue Using Array - Implement using linked list with dynamic memory allocation.
+
+// Input:
+// - First line: integer n (number of elements)
+// - Second line: n space-separated integers
+
+// Output:
+// - Print queue elements from front to rear, space-separated
+
+// Example:
+// Input:
+// 5
+// 10 20 30 40 50
+
+// Output:
+// 10 20 30 40 50
+
+// Explanation:
+// Use array and front/rear pointers. Enqueue inserts at rear, dequeue removes from front. Display from front to rear.
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// Global pointers
+Node* front = NULL;
+Node* rear = NULL;
+
+// Enqueue operation
+void enqueue(int value) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+
+    // If queue is empty
+    if (rear == NULL) {
+        front = rear = newNode;
+        return;
+    }
+
+    // Insert at rear
+    rear->next = newNode;
+    rear = newNode;
+}
+
+// Display queue
+void display() {
+    Node* temp = front;
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int x;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+
+    display();
+
+    return 0;
+}
