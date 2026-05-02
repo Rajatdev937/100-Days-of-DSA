bf5c8954efd9255470cb4cafa91e56debda620dc
Rajat Negi <007negirajat@gmail.com>
2026-05-02 23:48:39 +0530

    BSF

--- Day075.c
+++ Day075.c
@@ -16,7 +16,6 @@
 // Explanation
 // The subarray [-2, 2, -8, 1, 7] has a sum of 0 and is the longest such subarray.
 
-
 #include <stdio.h>
 
 #define MAX 10000
@@ -31,29 +30,26 @@ int maxLenZeroSum(int arr[], int n) {
         map[i] = -2;
     }
 
-    // Offset to handle negative prefix sums
     int offset = MAX;
 
     for (int i = 0; i < n; i++) {
         prefix += arr[i];
 
-        // Case 1: prefix sum is zero
         if (prefix == 0) {
             maxLen = i + 1;
         }
 
-        // Case 2: prefix seen before
         if (map[prefix + offset] != -2) {
             int len = i - map[prefix + offset];
             if (len > maxLen) {
                 maxLen = len;
             }
         } else {
-            // store first occurrence
+            
+
             map[prefix + offset] = i;
         }
     }
-
     return maxLen;
 }
 
@@ -61,7 +57,7 @@ int main() {
     int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    printf("%d\n", maxLenZeroSum(arr, n));  // Output: 5
+    printf("%d\n", maxLenZeroSum(arr, n)); 
 
     return 0;
 }
