e48553fe2c0f00171e49122a6abadd1bf74d66a9
Rajat Negi <007negirajat@gmail.com>
2026-05-05 13:48:13 +0530

    BSF

---
+++ Day077.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+struct Node* adj[MAX];
+int visited[MAX];
+
+// Create node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Add edge (undirected)
+void addEdge(int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = adj[u];
+    adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = adj[v];
+    adj[v] = newNode;
+}
+
+// DFS
+void dfs(int node) {
+    visited[node] = 1;
+
+    struct Node* temp = adj[node];
+    while (temp != NULL) {
+        if (!visited[temp->vertex]) {
+            dfs(temp->vertex);
+        }
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // Initialize
+    for (int i = 1; i <= n; i++) {
+        adj[i] = NULL;
+        visited[i] = 0;
+    }
+
+    // Input edges
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        addEdge(u, v);
+    }
+
+    // Start DFS from node 1
+    dfs(1);
+
+    // Check if all nodes visited
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) {
+            printf("NOT CONNECTED\n");
+            return 0;
+        }
+    }
+
+    printf("CONNECTED\n");
+    return 0;
+}
